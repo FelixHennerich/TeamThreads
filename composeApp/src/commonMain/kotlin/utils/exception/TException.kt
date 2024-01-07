b48f18e7e03e6ba209f4a32a950971d36367f27f
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2024-01-07 16:00:59 +0100

    exceptionhandler

---
+++ composeApp/src/commonMain/kotlin/utils/exception/TException.kt
@@ -0,0 +1,41 @@
+package utils.exception
+
+enum class TException {
+
+    SUCCESS001,
+
+    /**
+     * 100
+     */
+
+    Emailwrong100,
+    PasswordToWeak101,
+    UsernameLength102,
+    UsernameExists103,
+    EmailExists104,
+    UnallowedCharacters105,
+    BirthdayWrong106,
+    WrongPassword107,
+
+    LogoutError120,
+    LogoutWorked121,
+
+    /**
+     * 200
+     */
+
+    IsAlreadyFollowing200,
+    IsNotFollowing201,
+
+    /**
+     * 300
+     */
+
+    /**
+     * 400
+     */
+
+    HTTPPosting400,
+    DatabaseCreation401,
+
+}
